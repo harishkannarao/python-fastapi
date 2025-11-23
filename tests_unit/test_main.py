5073a11261b9c81138cda46eff2ac0488481e771
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-23 20:02:24 +0000

    Unit and integration test for context and router

--- tests_unit/test_main.py
+++ tests_unit/test_main.py
@@ -5,3 +5,9 @@ def test_root_get(test_client: TestClient):
     response = test_client.get("")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello Bigger Applications!"}
+
+
+def test_context_root_get(test_client: TestClient):
+    response = test_client.get("/context")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello Sub Application!"}
