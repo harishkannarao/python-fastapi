4685bb96d3f1cd5687f19e7abf16e19e8fb213bf
harishkannarao <harishkannarao@users.noreply.github.com>
2025-12-21 22:27:49 +0000

    Inserting customers

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -1,7 +1,14 @@
+from typing import Any
+
 from fastapi import APIRouter
 
 from app.db.database_config import database
 from app.model.customer import Customer
+
+SELECT_ALL_CUSTOMERS = "select * from customers"
+INSERT_CUSTOMER = (
+    "INSERT INTO customers(first_name, last_name) VALUES (:first_name, :last_name)"
+)
 
 router = APIRouter()
 
@@ -12,6 +19,12 @@ def to_customer(row) -> Customer:
 
 @router.get("/customers/", tags=["users"])
 async def read_customers() -> list[Customer]:
-    rows = await database.fetch_all(query="select * from customers")
+    rows = await database.fetch_all(query=SELECT_ALL_CUSTOMERS)
     customers: list[Customer] = list(map(to_customer, rows))
     return customers
+
+
+@router.put("/customers/", tags=["users"])
+async def insert_customers(customers: list[Customer]) -> None:
+    rows: list[dict[str, Any]] = list(map(lambda customer: vars(customer), customers))
+    await database.execute_many(query=INSERT_CUSTOMER, values=rows)
