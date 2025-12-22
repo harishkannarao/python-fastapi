c9ef75948e957b20afd3e607f7b78ec0b64652a8
harishkannarao <harishkannarao@users.noreply.github.com>
2025-12-22 17:39:35 +0000

    Using prefix

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -11,24 +11,24 @@ INSERT_CUSTOMER = (
 )
 DELETE_ALL_CUSTOMERS = "TRUNCATE TABLE CUSTOMERS"
 
-router = APIRouter()
+router = APIRouter(prefix="/customers", tags=["users"])
 
 
-@router.get("/customers/", tags=["users"])
+@router.get("")
 async def read_customers() -> list[Customer]:
     rows = await database.fetch_all(query=SELECT_ALL_CUSTOMERS)
     customers: list[Customer] = list(map(lambda row: Customer(**dict(row)), rows))
     return customers
 
 
-@router.put("/customers/", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
+@router.put("", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
 async def insert_customers(customers: list[Customer]) -> None:
     rows: list[dict[str, Any]] = list(map(lambda customer: vars(customer), customers))
     await database.execute_many(query=INSERT_CUSTOMER, values=rows)
     return None
 
 
-@router.delete("/customers/", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
+@router.delete("", status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
 async def delete_all_customers() -> None:
     await database.execute(query=DELETE_ALL_CUSTOMERS)
     return None
