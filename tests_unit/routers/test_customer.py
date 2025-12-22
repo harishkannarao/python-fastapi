47a92369bf88fc3546556c2d1abe8317374ed5e7
harishkannarao <harishkannarao@users.noreply.github.com>
2025-12-22 19:16:58 +0000

    Complete integration tests

--- tests_unit/routers/test_customer.py
+++ tests_unit/routers/test_customer.py
@@ -19,19 +19,23 @@ def mock_execute(mocker: MockerFixture) -> AsyncMock:
 
 @pytest.fixture
 def mock_execute_many(mocker: MockerFixture) -> AsyncMock:
-    mock_execute_many: AsyncMock = mocker.patch("app.db.database_config.database.execute_many")
+    mock_execute_many: AsyncMock = mocker.patch(
+        "app.db.database_config.database.execute_many"
+    )
     return mock_execute_many
 
 
 @pytest.fixture
 def mock_fetch_all(mocker: MockerFixture) -> AsyncMock:
-    mock_fetch_all: AsyncMock = mocker.patch("app.db.database_config.database.fetch_all")
+    mock_fetch_all: AsyncMock = mocker.patch(
+        "app.db.database_config.database.fetch_all"
+    )
     return mock_fetch_all
 
 
 def test_customers_delete(mock_execute: AsyncMock, test_client: TestClient):
-    initial_delete: Response = test_client.delete("/context/customers")
-    assert_that(initial_delete.status_code).is_equal_to(204)
+    response: Response = test_client.delete("/context/customers")
+    assert_that(response.status_code).is_equal_to(204)
 
     assert len(mock_execute.call_args_list) == 1
     assert mock_execute.call_args.kwargs["query"] == "TRUNCATE TABLE CUSTOMERS"
@@ -41,6 +45,7 @@ def test_customers_insert(mock_execute_many: AsyncMock, test_client: TestClient)
     customer1 = Customer(first_name="fname1", last_name="lname1")
     customer2 = Customer(first_name="fname2", last_name="lname2")
     input_json = jsonable_encoder([customer1, customer2])
+
     response: Response = test_client.put("/context/customers", json=input_json)
     assert_that(response.status_code).is_equal_to(204)
 
