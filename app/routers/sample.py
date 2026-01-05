1c063cbc2dfc04c1ac2aa3ed235eb6d8b2e67039
harishkannarao <harishkannarao@users.noreply.github.com>
2026-01-05 16:02:36 +0000

    insert using sqlmodel

--- app/routers/sample.py
+++ app/routers/sample.py
@@ -6,11 +6,21 @@ from sqlmodel import Session, select
 from app.db.database_config import get_session
 from app.model.db.sample_table import SampleTable
 
-router = APIRouter()
+router = APIRouter(prefix="/samples/sqlmodel", tags=["samples", "sqlmodel"])
 
 
-@router.get("/sample", tags=["sample"])
+@router.get("")
 async def read_all_samples(
     session: Session = Depends(get_session),
 ) -> Sequence[SampleTable]:
     return session.exec(select(SampleTable)).all()
+
+@router.put("")
+async def create_sample(
+        sample: SampleTable,
+    session: Session = Depends(get_session),
+) -> SampleTable:
+    session.add(sample)
+    session.commit()
+    session.refresh(sample)
+    return sample
