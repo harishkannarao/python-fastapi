770425f25402164da45de37efd4fff4df387d3eb
harishkannarao <harishkannarao@users.noreply.github.com>
2026-01-05 22:37:52 +0000

    update using sqlmodel

--- app/routers/sample.py
+++ app/routers/sample.py
@@ -1,7 +1,9 @@
 from typing import Sequence
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Response, Depends
+from sqlalchemy import ScalarResult
 from sqlmodel import Session, select
+from sqlmodel.sql._expression_select_cls import SelectOfScalar
 
 from app.db.database_config import get_session
 from app.model.db.sample_table import SampleTable
@@ -15,12 +17,36 @@ async def read_all_samples(
 ) -> Sequence[SampleTable]:
     return session.exec(select(SampleTable)).all()
 
+
 @router.put("")
 async def create_sample(
-        sample: SampleTable,
+    sample: SampleTable,
     session: Session = Depends(get_session),
 ) -> SampleTable:
     session.add(sample)
     session.commit()
     session.refresh(sample)
     return sample
+
+
+@router.post("")
+async def update_sample(
+    sample: SampleTable,
+    response: Response,
+    session: Session = Depends(get_session),
+) -> SampleTable | None:
+    statement: SelectOfScalar[SampleTable] = (
+        select(SampleTable)
+        .where(SampleTable.id == sample.id)
+        .where(SampleTable.version == sample.version)
+    )
+    results: ScalarResult[SampleTable] = session.exec(statement)
+    found_sample: SampleTable | None = results.first()
+    if found_sample is None:
+        response.status_code = 409
+        return None
+    found_sample.sqlmodel_update(sample.model_dump(exclude_unset=True))
+    session.add(found_sample)
+    session.commit()
+    session.refresh(found_sample)
+    return found_sample
