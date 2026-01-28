eb9d9826c1195804842908f9bafb7c7ad2f2b8d9
harishkannarao <harishkannarao@users.noreply.github.com>
2026-01-28 23:07:23 +0000

    verifying external http request

--- tests_integration/routers/test_external_faq.py
+++ tests_integration/routers/test_external_faq.py
@@ -1,8 +1,11 @@
+from typing import Any
+
 from assertpy import assert_that
 from fastapi.testclient import TestClient
 
 from httpx import Response
 from pytest_httpserver import HTTPServer
+from werkzeug import Request
 
 EXTERNAL_FAQ_ENDPOINT = "/context/external-faq"
 
@@ -11,7 +14,7 @@ def test_external_faq_get(
     mock_external_faq_server: HTTPServer,
     test_client: TestClient,
 ):
-    external_faq = [
+    external_faq: list[dict[str, Any]] = [
         {"name": "faq-1", "tag": "product", "id": 2},
         {"name": "faq-2", "tag": "product", "id": 3},
     ]
@@ -22,3 +25,8 @@ def test_external_faq_get(
     get_response: Response = test_client.get(EXTERNAL_FAQ_ENDPOINT)
     assert_that(get_response.status_code).is_equal_to(200)
     assert_that(get_response.json()).is_equal_to(external_faq)
+
+    requests_made: list[Request] = [req for req, res in mock_external_faq_server.log]
+    assert_that(requests_made).is_length(1)
+    assert_that(requests_made[0].path).is_equal_to("/faqs")
+    assert_that(requests_made[0].method).is_equal_to("GET")
