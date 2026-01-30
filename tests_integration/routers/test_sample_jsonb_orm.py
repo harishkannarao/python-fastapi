eb3b8cc4eb62f228faab042bf4c3041579531a52
harishkannarao <harishkannarao@users.noreply.github.com>
2026-01-30 16:28:22 +0000

    Jsonb read all

--- tests_integration/routers/test_sample_jsonb_orm.py
+++ tests_integration/routers/test_sample_jsonb_orm.py
@@ -43,6 +43,32 @@ def test_sample_jsonb_orm_create(delete_all_fixture: None, test_client: TestClie
     assert_created_response_entity(response_entity, request_entity)
 
 
+def test_sample_jsonb_orm_read_all(delete_all_fixture: None, test_client: TestClient):
+    sample1 = create_random_sample(test_client)
+    sample2 = create_random_sample(test_client)
+
+    sample_document_1 = create_random_sample_document(test_client, sample1.id)
+    sample_document_2 = create_random_sample_document(test_client, sample2.id)
+    http_response = test_client.get(SAMPLE_JSONB_ORM_ENDPOINT)
+    assert_that(http_response.status_code).is_equal_to(200)
+    all_documents = [SampleDocument(**item) for item in http_response.json()]
+    assert_that(all_documents).is_length(2)
+    assert_that(
+        [
+            sample_document
+            for sample_document in all_documents
+            if sample_document.id == sample_document_1.id
+        ]
+    ).contains_only(sample_document_1)
+    assert_that(
+        [
+            sample_document
+            for sample_document in all_documents
+            if sample_document.id == sample_document_2.id
+        ]
+    ).contains_only(sample_document_2)
+
+
 def delete_all(test_client: TestClient) -> None:
     delete_all_response: Response = test_client.delete(SAMPLE_JSONB_ORM_ENDPOINT)
     assert_that(delete_all_response.status_code).is_equal_to(204)
@@ -51,6 +77,21 @@ def delete_all(test_client: TestClient) -> None:
 def delete_all_samples(test_client: TestClient) -> None:
     delete_all_response: Response = test_client.delete(SAMPLE_ORM_ENDPOINT)
     assert_that(delete_all_response.status_code).is_equal_to(204)
+
+
+def create_random_sample_document(
+    test_client: TestClient, sample_id: uuid.UUID
+) -> SampleDocument:
+    request_entity = SampleDocumentCreate(
+        sample_id=sample_id,
+        json_data=DocumentMetadata(id=uuid.uuid4(), tags=[f"tag-{uuid.uuid4()}"]),
+        secondary_json_dict={"key": f"value-{uuid.uuid4()}"},
+    )
+    http_response = test_client.put(
+        SAMPLE_JSONB_ORM_ENDPOINT, json=jsonable_encoder(request_entity)
+    )
+    assert_that(http_response.status_code).is_equal_to(200)
+    return SampleDocument(**http_response.json())
 
 
 def create_random_sample(test_client: TestClient) -> Sample:
