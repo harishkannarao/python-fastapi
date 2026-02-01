5494b3e6f841b6fa5704efe6a9df539fed132eaf
harishkannarao <harishkannarao@users.noreply.github.com>
2026-02-01 09:01:09 +0000

    Moved all operations to dao

--- app/dao/sample_orm_dao.py
+++ app/dao/sample_orm_dao.py
@@ -3,11 +3,12 @@ from datetime import datetime, timezone
 from uuid import UUID
 
 from pydantic import RootModel
-from sqlmodel import select, asc
+from sqlalchemy import ScalarResult
+from sqlmodel import select, delete, asc, Session
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.model.entity.sample_entity import SampleEntity
-from app.model.request.sample import SampleCreate
+from app.model.request.sample import SampleCreate, SampleUpdate
 from app.model.response.sample import Sample
 
 
@@ -45,3 +46,38 @@ async def create_sample(session: AsyncSession, sample: SampleCreate) -> Sample:
     await session.flush()
     await session.refresh(sample_entity)
     return Sample(**sample_entity.model_dump())
+
+
+def update_sample(session: Session, sample: SampleUpdate) -> Sample | None:
+    statement = (
+        select(SampleEntity)
+        .where(SampleEntity.id == sample.id)
+        .where(SampleEntity.version == sample.old_version)
+    )
+    results: ScalarResult[SampleEntity] = session.exec(statement)
+    found_sample: SampleEntity | None = results.one_or_none()
+    if found_sample is None:
+        return None
+    found_sample.sqlmodel_update(RootModel(sample).model_dump())
+    found_sample.updated_datetime = datetime.now(timezone.utc)
+    found_sample.version = sample.new_version
+    session.add(found_sample)
+    session.flush()
+    session.refresh(found_sample)
+    return Sample(**found_sample.model_dump())
+
+
+async def delete_sample_by_id(session: AsyncSession, sample_id: UUID) -> None:
+    result: SampleEntity | None = (
+        await session.exec(select(SampleEntity).where(SampleEntity.id == sample_id))
+    ).one_or_none()
+    if result is not None:
+        await session.delete(result)
+        await session.flush()
+    return None
+
+
+def delete_all(session: Session) -> None:
+    session.exec(delete(SampleEntity))
+    session.flush()
+    return None
--- app/routers/sample_orm.py
+++ app/routers/sample_orm.py
@@ -1,16 +1,18 @@
-from datetime import datetime, timezone
 from uuid import UUID
 
 from fastapi import APIRouter, Query, Response
-from pydantic import RootModel
-from sqlalchemy import ScalarResult
-from sqlmodel import select, delete
 
 from app.db.database_dependencies import AsyncSessionDep, SessionDep
-from app.model.entity.sample_entity import SampleEntity
 from app.model.request.sample import SampleCreate, SampleUpdate
 from app.model.response.sample import Sample
-from app.dao.sample_orm_dao import read_samples, read_sample_by_id, create_sample
+from app.dao.sample_orm_dao import (
+    read_samples,
+    read_sample_by_id,
+    create_sample,
+    update_sample,
+    delete_sample_by_id,
+    delete_all,
+)
 
 router = APIRouter(prefix="/samples/orm", tags=["samples", "orm"])
 
@@ -43,41 +45,22 @@ async def create_sample_handler(
 
 
 @router.post("")
-async def update_sample(
+async def update_sample_handler(
     session: SessionDep, sample: SampleUpdate, response: Response
 ) -> Sample | None:
-    statement = (
-        select(SampleEntity)
-        .where(SampleEntity.id == sample.id)
-        .where(SampleEntity.version == sample.old_version)
-    )
-    results: ScalarResult[SampleEntity] = session.exec(statement)
-    found_sample: SampleEntity | None = results.one_or_none()
-    if found_sample is None:
+    result: Sample | None = update_sample(session, sample)
+    if result is None:
         response.status_code = 409
-        return None
-    found_sample.sqlmodel_update(RootModel(sample).model_dump())
-    found_sample.updated_datetime = datetime.now(timezone.utc)
-    found_sample.version = sample.new_version
-    session.add(found_sample)
-    session.flush()
-    session.refresh(found_sample)
-    return Sample(**found_sample.model_dump())
+    return result
 
 
 @router.delete("/{sample_id}", status_code=204)
-async def delete_sample_by_id(session: AsyncSessionDep, sample_id: UUID) -> None:
-    result: SampleEntity | None = (
-        await session.exec(select(SampleEntity).where(SampleEntity.id == sample_id))
-    ).one_or_none()
-    if result is not None:
-        await session.delete(result)
-        await session.flush()
-    return None
+async def delete_sample_by_id_handler(
+    session: AsyncSessionDep, sample_id: UUID
+) -> None:
+    return await delete_sample_by_id(session, sample_id)
 
 
 @router.delete("", status_code=204)
-async def delete_all(session: SessionDep) -> None:
-    session.exec(delete(SampleEntity))
-    session.flush()
-    return None
+async def delete_all_handler(session: SessionDep) -> None:
+    return delete_all(session)
