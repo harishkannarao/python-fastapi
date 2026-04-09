f444686b1edeec5038693d8340f09e7966118f68
harishkannarao <harishkannarao@users.noreply.github.com>
2026-04-09 14:30:13 +0100

    First sql dao test

---
+++ app/dao/sample_sql_dao.py
@@ -0,0 +1,30 @@
+import datetime
+import uuid
+
+from app.db.database_config import database
+from app.model.request.sample import SampleCreate
+from app.model.response.sample import Sample
+
+READ_SAMPLE_BY_ID = (
+    "select * from sample_table where id=:sample_id"
+)
+
+INSERT_SAMPLE = (
+    """INSERT INTO 
+    sample_table(id, username, version, created_datetime, updated_datetime) 
+    VALUES 
+    (:sample_id, :username, 1, timezone('utc', now()), timezone('utc', now()))"""
+)
+
+async def read_sample_by_id(sample_id: uuid.UUID) -> Sample | None:
+    row = await database.fetch_one(query=READ_SAMPLE_BY_ID, values={"sample_id": str(sample_id)})
+    if row:
+        return Sample(**dict(row))
+    else:
+        return None
+
+async def create_sample(sample: SampleCreate) -> Sample | None:
+    sample_id: uuid.UUID = uuid.uuid4()
+    input_dict = {"sample_id": str(sample_id), "username": sample.username}
+    await database.execute(query=INSERT_SAMPLE, values=input_dict)
+    return await read_sample_by_id(sample_id)
---
+++ tests_integration/dao/test_sample_sql_dao.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timezone, timedelta
+import uuid
+from decimal import Decimal
+
+from assertpy import assert_that
+import pytest
+import pytest_asyncio
+from databases import Database
+from pytest import MonkeyPatch
+
+from app.dao import sample_sql_dao
+from app.model.request.sample import SampleCreate
+from app.model.response.sample import Sample
+
+
+@pytest_asyncio.fixture(autouse=True)
+def setup_dao_database(get_database: Database, monkeypatch: MonkeyPatch) -> None:
+    monkeypatch.setattr("app.dao.sample_sql_dao.database", get_database)
+
+
+@pytest.mark.asyncio
+async def test_sample_create_and_read():
+    start_time: datetime = datetime.now(tz=timezone.utc)
+    request: SampleCreate = SampleCreate(
+        username=f"usr-{uuid.uuid4()}",
+        bool_field=True,
+        float_field=2.1,
+        decimal_field=Decimal(3.4)
+    )
+    create_result: Sample = await sample_sql_dao.create_sample(request)
+
+    assert_that(create_result.id).is_not_none()
+    assert_that(create_result.username).is_equal_to(request.username)
+    # assert_that(create_result.bool_field).is_equal_to(request.bool_field)
+    # assert_that(create_result.float_field).is_equal_to(request.float_field)
+    # assert_that(create_result.decimal_field).is_equal_to(request.decimal_field)
+    assert_that(create_result.created_datetime).is_greater_than_or_equal_to(start_time)
+    assert_that(create_result.created_datetime).is_less_than_or_equal_to(datetime.now(tz=timezone.utc) + timedelta(seconds=2))
+    assert_that(create_result.updated_datetime).is_greater_than_or_equal_to(start_time)
+    assert_that(create_result.updated_datetime).is_less_than_or_equal_to(datetime.now(tz=timezone.utc) + timedelta(seconds=2))
+
