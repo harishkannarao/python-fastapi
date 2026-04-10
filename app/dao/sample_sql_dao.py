43137323240a91d8fceab36728dd2095e2da6aca
harishkannarao <harishkannarao@users.noreply.github.com>
2026-04-10 09:23:59 +0100

    Further formatting

--- app/dao/sample_sql_dao.py
+++ app/dao/sample_sql_dao.py
@@ -1,15 +1,32 @@
 import uuid
 
 from app.db.database_config import database
-from app.model.request.sample import SampleCreate
+from app.model.request.sample import SampleCreate, SampleUpdate
 from app.model.response.sample import Sample
 
 READ_SAMPLE_BY_ID = "select * from sample_table where id=:sample_id"
 
-INSERT_SAMPLE = """INSERT INTO
-     sample_table(id, username, bool_field, float_field, decimal_field, version, created_datetime, updated_datetime)
-     VALUES
-     (:sample_id, :username, :bool_field, :float_field, :decimal_field, 1, timezone('utc', now()), timezone('utc', now()))"""
+INSERT_SAMPLE = """
+    INSERT INTO
+    sample_table(
+        id, username, bool_field, float_field, decimal_field, version, created_datetime, updated_datetime
+    ) VALUES(
+        :id, :username, :bool_field, :float_field,
+        :decimal_field, 1, timezone('utc', now()), timezone('utc', now())
+    )
+    """
+
+UPDATE_SAMPLE = """
+    UPDATE sample_table SET
+    username = :username,
+    bool_field = :bool_field,
+    float_field = :float_field,
+    decimal_field = :decimal_field,
+    version = :new_version,
+    updated_datetime = timezone('utc', now())
+    WHERE
+    id = :id AND version = :old_version
+    """
 
 
 async def read_sample_by_id(sample_id: uuid.UUID) -> Sample | None:
@@ -25,6 +42,10 @@ async def read_sample_by_id(sample_id: uuid.UUID) -> Sample | None:
 async def create_sample(sample: SampleCreate) -> uuid.UUID:
     sample_id: uuid.UUID = uuid.uuid4()
     input_dict = vars(sample)
-    input_dict["sample_id"] = sample_id
+    input_dict["id"] = sample_id
     await database.execute(query=INSERT_SAMPLE, values=input_dict)
     return sample_id
+
+
+async def update_sample(sample: SampleUpdate) -> None:
+    return None
