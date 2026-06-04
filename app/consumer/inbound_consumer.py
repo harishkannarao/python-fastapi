239beb7834a82e020f15375602933aa1be72543f
harishkannarao <harishkannarao@users.noreply.github.com>
2026-06-04 22:03:39 +0100

    fixed logging

--- app/consumer/inbound_consumer.py
+++ app/consumer/inbound_consumer.py
@@ -5,15 +5,14 @@ from _asyncio import Task
 from typing import Any
 
 import structlog
-from aio_pika.abc import AbstractIncomingMessage, HeadersType
+from aio_pika.abc import AbstractIncomingMessage
 from fastapi.encoders import jsonable_encoder
 
-from app.model.response.sample import Sample
-from app.rabbit_mq.rabbit_mq_client import get_connection
 from app.config import settings
-
-from app.producer.outbound_producer import publish_to_outbound
+from app.model.response.sample import Sample
 from app.producer.inbound_retry_producer import publish_to_inbound_retry
+from app.producer.outbound_producer import publish_to_outbound
+from app.rabbit_mq.rabbit_mq_client import get_connection
 
 
 async def process_inbound_message_task(message: AbstractIncomingMessage):
@@ -24,33 +23,35 @@ async def process_inbound_message_task(message: AbstractIncomingMessage):
             headers = dict(message.headers) if message.headers else {}
             count: int = headers.get("count", 1)
             message_id: str = headers.get("message_id", str(uuid.uuid4()))
-            message_context: dict[str, Any] = {
-                "message_id": message_id,
-                "count": count
-            }
-            structlog.contextvars.bind_contextvars(**message_context)
-            logger.info("Test logging")
-            logger.info(
+            message_context: dict[str, Any] = {"message_id": message_id, "count": count}
+            custom_logger = logger.bind(**message_context)
+            custom_logger.info(
                 f"Received inbound message {payload_string}",
                 payload=payload_string,
                 headers=headers,
             )
             samples = [Sample(**item) for item in json.loads(payload_string)]
             await publish_to_outbound(samples=samples, headers=headers)
-            logger.info(
+            custom_logger.info(
                 f"Processed inbound message {payload_string}",
                 headers=headers,
                 samples=jsonable_encoder(samples),
             )
         except Exception as e:
-            logger.error(f"Unexpected Exception!: {repr(e)}")
-            headers.update({"count": count + 1, "message_id": message_id})
+            logger.error(
+                f"Unexpected Exception!: {repr(e)}",
+                payload_string=payload_string,
+                headers=headers,
+            )
             if samples is None:
-               logger.error(f"Unable to parse raw payload {payload_string}")
+                logger.error(
+                    f"Unable to parse raw payload {payload_string}",
+                    payload_string=payload_string,
+                    headers=headers,
+                )
             else:
+                headers.update({"count": count + 1, "message_id": message_id})
                 await publish_to_inbound_retry(samples=samples, headers=headers)
-        finally:
-            structlog.contextvars.clear_contextvars()
 
 
 async def start_inbound_consumer() -> Task[str]:
