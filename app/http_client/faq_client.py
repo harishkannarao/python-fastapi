5be8069660c4cd784171ec6a70076fe1c790106e
harishkannarao <harishkannarao@users.noreply.github.com>
2026-06-07 22:22:28 +0100

    Using httpx2 library

--- app/http_client/faq_client.py
+++ app/http_client/faq_client.py
@@ -1,18 +1,18 @@
 from typing import Any
 
-import httpx
+import httpx2
 
 from app.config import settings
 
 
 async def get_faqs() -> Any:
-    async with httpx.AsyncClient() as client:
+    async with httpx2.AsyncClient() as client:
         response = await client.get(f"{settings.app_external_faq_api_base_url}/faqs")
         return response.json()
 
 
 async def get_faqs_by_id(faq_id: int) -> Any:
-    async with httpx.AsyncClient() as client:
+    async with httpx2.AsyncClient() as client:
         response = await client.get(
             f"{settings.app_external_faq_api_base_url}/faqs/{faq_id}"
         )
@@ -20,7 +20,7 @@ async def get_faqs_by_id(faq_id: int) -> Any:
 
 
 def create_faq(faq: dict[str, Any]) -> Any:
-    with httpx.Client() as client:
+    with httpx2.Client() as client:
         response = client.post(
             f"{settings.app_external_faq_api_base_url}/faqs", json=faq
         )
